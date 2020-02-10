dd8bc2132710871d2bd7502b4e7699267da643b3
Seokgyu Lee <dltjrrb0729@naver.com>
2020-02-10 23:43:36 +0900

    finished without the last part

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,7 @@
-import { getMovies, getById, addMovie, deleteMovies } from "./db";
+import { getMovies } from "./db";
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovies: (_, { id }) => deleteMovies(id)
+    movies: (_, { limit, rating }) => getMovies(limit, rating)
   }
 };
 
